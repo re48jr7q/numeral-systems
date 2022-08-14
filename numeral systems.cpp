944e5b17bb37370ec0847fa74323cc90ae3cd777
re48jr7q <erwin.degeus45@gmail.com>
2022-08-14 23:57:57 +0200

    Add project files.

---
+++ numeral systems.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int bin{};		//assume a 16 bit int
+
+	//bin = 0x0001;
+	//bin = 0x00FF;
+	//bin = 0x00B3;
+	bin = 0xF770;
+
+	std::cout << bin;
+
+	return 0;
+}
+
